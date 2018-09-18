3e24cd1c1da4125701a0a83129e9222239a0b943
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2018-09-18 09:36:38 +0900

    First Commit

---
+++ AzureAD-V2-ASPmvcWebApp/AzureAD-V2-ASPmvcWebApp/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(AzureAD_V2_ASPmvcWebApp.Startup))]
+namespace AzureAD_V2_ASPmvcWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
