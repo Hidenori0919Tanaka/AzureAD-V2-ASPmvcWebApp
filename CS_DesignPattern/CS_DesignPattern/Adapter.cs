f0e3f3a867b9a627f3cecdaee796d8affcd263cd
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2018-11-21 12:51:24 +0900

    first commit

---
+++ CS_DesignPattern/CS_DesignPattern/Adapter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CS_DesignPattern
+{
+    internal class Adapter
+    {
+        public Adapter()
+        {
+        }
+
+        public void Main()
+        {
+            Print p = new PrintBanner("Hello");
+            p.PrintWeak();
+            // => (Hello)
+            p.PrintStrong();
+            // => *Hello*
+
+            // 実行が一瞬で終わって確認できないので、キーの入力を待ちます
+            Console.ReadLine();
+        }
+    }
+
+    // このクラスは既に提供されているものとします
+    // Adaptee
+    public class Banner
+    {
+        private string str;
+        public Banner(string str)
+        {
+            this.str = str;
+        }
+        public void ShowWithPattern()
+        {
+            Console.WriteLine($"({str})");
+        }
+        public void ShowWithAster()
+        {
+            Console.WriteLine($"*{str}*");
+        }
+    }
+
+    // Target
+    public interface Print
+    {
+        void PrintWeak();
+        void PrintStrong();
+    }
+
+    // Adapter
+    public class PrintBanner : Banner, Print
+    {
+        public PrintBanner(string str) : base(str) { }
+        public void PrintWeak()
+        {
+            ShowWithPattern();
+        }
+        public void PrintStrong()
+        {
+            ShowWithAster();
+        }
+    }
+
+}
---
+++ CS_DesignPattern/CS_DesignPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DesignPattern
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            new IteratorPattern().Main();
+            new Adapter().Main();
+        }
+    }
+}
